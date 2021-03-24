e34435f81a1314a84740eebffd9cdae7bec1885f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-03-24 15:43:45 +0900

    Create resize.sh

---
+++ resize.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+rm -f ./storage/dcim/Automate/carnavi.png
+convert ./storage/dcim/Automate/screenshot.png -resize 30% ./storage/dcim/Automate/carnavi.png
+rm -f ./storage/dcim/Automate/screenshot.png
