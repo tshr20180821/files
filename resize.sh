166b069f1855f35434b949242769b9fa69260491
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-03-24 15:46:43 +0900

    Delete resize.sh

--- resize.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-
-rm -f ./storage/dcim/Automate/carnavi.png
-convert ./storage/dcim/Automate/screenshot.png -resize 30% ./storage/dcim/Automate/carnavi.png
-rm -f ./storage/dcim/Automate/screenshot.png
