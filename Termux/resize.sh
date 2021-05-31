f65f99ced7d608f3011f9c90b4f410757935678b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2021-06-01 08:05:05 +0900

    Update resize.sh

--- Termux/resize.sh
+++ Termux/resize.sh
@@ -1,5 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+# /data/data/com.termux/files/home/.shortcuts
+
 convert ./storage/dcim/Automate/screenshot.png -resize 30% ./storage/dcim/Automate/carnavi.webp
 curl -sS -m 30 -u user:password -X POST -F carnavi=@./storage/dcim/Automate/carnavi.webp https://xxx.herokuapp.com/xxx.php &
 exit
